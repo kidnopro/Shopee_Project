86e12ced918b18af19b15cc2371c4cf0fee559a4
kidnopro <hoanganhfullstack@gmail.com>
2024-10-30 15:38:44 +0700

    Fix lại Header,Tạo Popover component custom

---
+++ src/components/Popover/Popover.tsx
@@ -0,0 +1,62 @@
+import { useRef, useState, useId, ElementType } from 'react'
+import { useFloating, arrow, offset, shift } from '@floating-ui/react-dom-interactions'
+import { FloatingPortal } from '@floating-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+interface Props {
+  children: React.ReactNode
+  renderPopover: React.ReactNode
+  className?: string
+  as?: ElementType
+}
+
+export default function Popover({ children, className, renderPopover, as: Element = "div" }: Props) {
+  const [open, setOpen] = useState(true)
+  const arrowRef = useRef<HTMLElement>(null)
+  const { x, y, reference, floating, strategy, middlewareData } = useFloating({
+    middleware: [offset(6), shift(), arrow({ element: arrowRef })]
+  })
+  const id = useId()
+  const showPopover = () => {
+    setOpen(true)
+  }
+  const hidePopover = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div className={className} ref={reference} onMouseEnter={showPopover} onMouseLeave={hidePopover}>
+      {children}
+      <FloatingPortal id={id}>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              ref={floating}
+              style={{
+                position: strategy,
+                top: y ?? 0,
+                left: x ?? 0,
+                width: 'max-content',
+                transformOrigin: `${middlewareData.arrow?.x}px top`
+              }}
+              initial={{ opacity: 0, transform: 'scale(0)' }}
+              animate={{ opacity: 1, transform: 'scale(1)' }}
+              exit={{ opacity: 0, transform: 'scale(0)' }}
+              transition={{ duration: 0.2 }}
+            >
+              <span
+                ref={arrowRef}
+                className='border-x-transparent border-t-transparent border-b-white border-[11px] absolute translate-y-[-95%] z-10'
+                style={{
+                  left: middlewareData.arrow?.x,
+                  top: middlewareData.arrow?.y
+                }}
+              />
+              {renderPopover}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </FloatingPortal>
+    </div>
+  )
+}
