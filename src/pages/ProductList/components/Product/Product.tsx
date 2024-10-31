445af9bfd16acba1cafc7098b94bfc4dab24f052
kidnopro <hoanganhfullstack@gmail.com>
2024-11-01 02:38:04 +0700

    Test login auth,deloy

--- src/pages/ProductList/components/Product/Product.tsx
+++ src/pages/ProductList/components/Product/Product.tsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom'
+
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100 transition-transform'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://tinhayvip.com/wp-content/uploads/2022/01/hot-girl-nguc-khung-lo-anh-khoa-than-khong-che-5.jpg'
+            alt='product'
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+      </div>
+    </Link>
+  )
 }
