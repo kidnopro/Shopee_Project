14dff7ef8a7dddf1f3c3d11c084f1045a89a1388
kidnopro <hoanganhfullstack@gmail.com>
2024-11-01 22:38:23 +0700

    Done phân trang

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,12 +1,15 @@
 import classNames from 'classnames'
+import { QueryConfig } from '../../pages/ProductList/ProductList'
+import { Link, createSearchParams } from 'react-router-dom'
+import path from '../../constants/path'
 
 interface Props {
-  page: number
-  setPage: React.Dispatch<React.SetStateAction<number>>
+  queryConfig: QueryConfig
   pageSize: number
 }
 const RANGE = 2
-export default function Pagination({ page, setPage, pageSize }: Props) {
+export default function Pagination({ queryConfig, pageSize }: Props) {
+  const page = Number(queryConfig.page)
   const renderPagination = () => {
     let dotAfter = false
     let dotBefore = false
@@ -14,9 +17,9 @@ export default function Pagination({ page, setPage, pageSize }: Props) {
       if (!dotBefore) {
         dotBefore = true
         return (
-          <button key={index} className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>
+          <span key={index} className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>
             ...
-          </button>
+          </span>
         )
       }
       return null
@@ -25,9 +28,9 @@ export default function Pagination({ page, setPage, pageSize }: Props) {
       if (!dotAfter) {
         dotAfter = true
         return (
-          <button key={index} className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>
+          <span key={index} className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>
             ...
-          </button>
+          </span>
         )
       }
       return null
@@ -48,25 +51,103 @@ export default function Pagination({ page, setPage, pageSize }: Props) {
           return renderDotBefore(index)
         }
         return (
-          <button
+          <Link
+            to={{
+              pathname: path.home,
+              search: createSearchParams({
+                ...queryConfig,
+                page: pageNumber.toString()
+              }).toString()
+            }}
             key={index}
             className={classNames('bg-white rounded px-3 py-3 mx-2 cursor-pointer border', {
               'border-cyan-500': pageNumber === page,
               'border-transparent': pageNumber !== page
             })}
-            onClick={() => setPage(pageNumber)}
           >
             {pageNumber}
-          </button>
+          </Link>
         )
       })
   }
 
   return (
-    <div className='flex flex-wrap mt-6'>
-      <button className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>Prev</button>
+    <div className='mt-6 flex flex-wrap justify-center'>
+      {page === 1 ? (
+        <span className='mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2  shadow-sm'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5' />
+          </svg>
+        </span>
+      ) : (
+        <Link
+          to={{
+            pathname: path.home,
+            search: createSearchParams({
+              ...queryConfig,
+              page: (page - 1).toString()
+            }).toString()
+          }}
+          className='mx-2 cursor-pointer rounded border bg-white px-3 py-2  shadow-sm'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5' />
+          </svg>
+        </Link>
+      )}
+
       {renderPagination()}
-      <button className='bg-white rounded px-3 py-3 mx-2 cursor-pointer border'>Next</button>
+      {page === pageSize ? (
+        <span className='mx-2 cursor-not-allowed rounded border bg-white/60 px-3 py-2  shadow-sm'>
+          {' '}
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-5 w-5'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5' />
+          </svg>
+        </span>
+      ) : (
+        <Link
+          to={{
+            pathname: path.home,
+            search: createSearchParams({
+              ...queryConfig,
+              page: (page + 1).toString()
+            }).toString()
+          }}
+          className='mx-2 cursor-pointer rounded border bg-white px-3 py-2  shadow-sm'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-5 h-5'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' d='m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5' />
+          </svg>
+        </Link>
+      )}
     </div>
   )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,39 +5,62 @@ import { useQuery } from '@tanstack/react-query'
 import useQueryParams from '../../hooks/useQueryParams'
 import productApi from '../../apis/product.api'
 import Pagination from '../../components/Pagination/Pagination'
-import { useState } from 'react'
+import { omitBy, isUndefined } from 'lodash'
+import { ProductListConfig } from '../../types/product.type'
+
+export type QueryConfig = {
+  [key in keyof ProductListConfig]: string
+}
 
 export default function ProductList() {
   const queryParams = useQueryParams()
-  const [page, setPage] = useState(1)
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryParams.page || '1',
+      limit: queryParams.limit || 5,
+      sort_by: queryParams.sort_by,
+      exclude: queryParams.exclude,
+      name: queryParams.name,
+      order: queryParams.order,
+      price_max: queryParams.price_max,
+      price_min: queryParams.price_min,
+      rating_filter: queryParams.rating_filter
+    },
+    isUndefined
+  )
+
   const { data } = useQuery({
-    queryKey: ['products', queryParams],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProduct(queryParams)
-    }
+      return productApi.getProduct(queryConfig as ProductListConfig)
+    },
+    // @ts-ignore
+    keepPreviousData: true,
+    staleTime: 3 * 60 * 1000
   })
   // console.log(data)
 
   return (
     <div className='bg-gray-200 py-6 h-[700px]'>
       <div className='container'>
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3'>
-            <AsideFilter />
-          </div>
-          <div className='col-span-9'>
-            <SortProductList />
-            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {data &&
-                data.data.data.products.map((product) => (
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              <AsideFilter />
+            </div>
+            <div className='col-span-9'>
+              <SortProductList />
+              <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+                {data.data.data.products.map((product) => (
                   <div className='col-span-1'>
                     <Product product={product} />
                   </div>
                 ))}
+              </div>
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
             </div>
-            <Pagination page={page} setPage={setPage} pageSize={20} />
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
