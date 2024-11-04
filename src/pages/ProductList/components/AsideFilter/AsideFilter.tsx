2a0393ba12ddc1617adf2418c6b37de352361190
kidnopro <hoanganhfullstack@gmail.com>
2024-11-04 17:02:45 +0700

    Rating start, clear filter

--- src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
@@ -1,25 +1,28 @@
-import { createSearchParams, Link, useNavigate } from 'react-router-dom'
-import path from '../../../../constants/path'
-import Button from '../../../../components/Button/Button'
-import { QueryConfig } from '../../ProductList'
-import { Category } from '../../../../types/category.type'
 import classNames from 'classnames'
-import InputNumber from '../../../../components/InputNumber/InputNumber'
+import { createSearchParams, Link, useNavigate } from 'react-router-dom'
 import { useForm, Controller } from 'react-hook-form'
-import { Schema, schema } from '../../../../utils/rule'
+import omit from 'lodash/omit'
+import { Category } from '../../../../types/category.type'
+import Button from '../../../../components/Button'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { NoUnderfinedField } from '../../../../types/utils.type'
+import RatingStarts from '../RatingStart'
+import { QueryConfig } from '../../../../hooks/useQueryConfig'
+import { schema, Schema } from '../../../../utils/rule'
+import path from '../../../../constants/path'
+import InputNumber from '../../../../components/InputNumber/InputNumber'
 
 interface Props {
   queryConfig: QueryConfig
   categories: Category[]
 }
 
-type FormData = NoUnderfinedField<Pick<Schema, 'price_min' | 'price_max'>>
+type FormData = NoUnderfinedField<Pick<Schema, 'price_max' | 'price_min'>>
 
 const priceSchema = schema.pick(['price_min', 'price_max'])
 
 export default function AsideFilter({ queryConfig, categories }: Props) {
+  // const { t } = useTranslation("home");
   const { category } = queryConfig
   const {
     control,
@@ -32,27 +35,32 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
       price_max: ''
     },
     // @ts-ignore
-    resolver: yupResolver(priceSchema),
-    shouldFocusError: false
+    resolver: yupResolver(priceSchema)
   })
   const navigate = useNavigate()
-
   const onSubmit = handleSubmit((data) => {
     navigate({
       pathname: path.home,
       search: createSearchParams({
         ...queryConfig,
-        price_min: data.price_min,
-        price_max: data.price_max
+        price_max: data.price_max,
+        price_min: data.price_min
       }).toString()
     })
   })
+
+  const handleRemoveAll = () => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(omit(queryConfig, ['price_min', 'price_max', 'rating_filter', 'category'])).toString()
+    })
+  }
 
   return (
     <div className='py-4 ml-14'>
       <Link
         to={path.home}
-        className={classNames('flex items-center ', {
+        className={classNames('flex items-center font-bold', {
           'text-orange-500': !category
         })}
       >
@@ -69,132 +77,121 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
             </g>
           </g>
         </svg>
-        Tất cả danh mục
+        {'aside filter.all categories'}
       </Link>
-      <div className='bg-gray-400 h-[1px] my-4'>
-        <ul>
-          {categories.map((categoryItem) => {
-            const isActive = category === categoryItem._id
-            return (
-              <li className='py-2 pl-2' key={categoryItem._id}>
-                <Link
-                  to={{
-                    pathname: path.home,
-                    search: createSearchParams({
-                      ...queryConfig,
-                      category: categoryItem._id
-                    }).toString()
-                  }}
-                  className={classNames('relative px-2', {
-                    'font-semibold text-orange-500': isActive
-                  })}
-                >
-                  {isActive && (
-                    <svg viewBox='0 0 4 7' className='absolute top-1 left-[-10px] h-2 w-2 fill-orange-500'>
-                      <polygon points='4 3.5 0 0 0 7' />
-                    </svg>
-                  )}
-                  {categoryItem.name}
-                </Link>
-              </li>
-            )
-          })}
-
-          <li className='py-2 pl-2'>
-            <Link to={path.home} className='relative px-2'>
-              Thời trang nữ
-            </Link>
-          </li>
-        </ul>
-        <Link to={path.home} className='flex items-center  mt-4 capitalize'>
-          <svg
-            enableBackground='new 0 0 15 15'
-            viewBox='0 0 15 15'
-            x={0}
-            y={0}
-            className='mr-3 h-4 w-3 fill-current stroke-current'
-          >
-            <g>
-              <polyline
-                fill='none'
-                points='5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeMiterlimit={10}
-              />
-            </g>
-          </svg>
-          Bộ lọc tìm kiếm
-        </Link>
-        <div className='bg-gray-400 h-[1px] my-4'></div>
-        <div className='my-5'>
-          <div>Khoản giá</div>
-          <form className='mt-2' onSubmit={onSubmit}>
-            <div className='flex items-start'>
-              <Controller
-                control={control}
-                name='price_min'
-                render={({ field }) => {
-                  return (
-                    <InputNumber
-                      type='text'
-                      className='grow'
-                      placeholder='đ Từ'
-                      {...field}
-                      classNameInput='p-1 w-full outline-none border boder-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                      classNameError='hidden'
-                      onChange={(event) => {
-                        field.onChange(event)
-                        trigger('price_max')
-                      }}
-                    />
-                  )
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <ul>
+        {categories.map((categoryItem) => {
+          const isActive = category === categoryItem._id
+          return (
+            <li className='py-2 pl-2' key={categoryItem._id}>
+              <Link
+                to={{
+                  pathname: path.home,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    category: categoryItem._id
+                  }).toString()
                 }}
-              />
-
-              <div className='mx-2 mt-2 shrink-0'>-</div>
-              <Controller
-                control={control}
-                name='price_max'
-                render={({ field }) => {
-                  return (
-                    <InputNumber
-                      type='text'
-                      className='grow'
-                      placeholder='đ Đến'
-                      classNameError='hidden'
-                      classNameInput='p-1 w-full outline-none border boder-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                      onChange={(event) => {
-                        field.onChange(event)
-                        trigger('price_max')
-                      }}
-                    />
-                  )
-                }}
-              />
-            </div>
-            <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm text-center'>{errors.price_min?.message}</div>
-            <Button className='w-full p-2 uppercase bg-orange-500 text-white text-sm hover:bg-orange-400 rounded-sm flex justify-center items-center'>
-              áp dụng
-            </Button>
-          </form>
-          <div className='bg-gray-400 h-[1px] my-4'></div>
-          <div className='text-sm'>Đánh giá</div>
-          <ul className=''>
-            <li className='py-1 pl-2'>
-              <Link to='' className='flex items-center text-sm'>
-                {/* {Array(5).fill(0).map((_, index) => (
-                    
-                ))} */}
+                className={classNames('relative px-2', {
+                  'font-semibold text-orange-500': isActive
+                })}
+              >
+                {isActive && (
+                  <svg viewBox='0 0 4 7' className='absolute top-1 left-[-10px] h-2 w-2 fill-orange-500'>
+                    <polygon points='4 3.5 0 0 0 7' />
+                  </svg>
+                )}
+                {categoryItem.name}
               </Link>
             </li>
-          </ul>
-          <div className='bg-gray-400 h-[1px] my-4'></div>
-          <Button className='w-full py-2 px-2 uppercase bg-orange-500 hover:bg-orange-400 text-sm text-white rounded-sm'>
-            Xóa tất cả
+          )
+        })}
+      </ul>
+      <Link to={path.home} className='mt-4 flex items-center font-bold uppercase'>
+        <svg
+          enableBackground='new 0 0 15 15'
+          viewBox='0 0 15 15'
+          x={0}
+          y={0}
+          className='mr-3 h-4 w-3 fill-current stroke-current'
+        >
+          <g>
+            <polyline
+              fill='none'
+              points='5.5 13.2 5.5 5.8 1.5 1.2 13.5 1.2 9.5 5.8 9.5 10.2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeMiterlimit={10}
+            />
+          </g>
+        </svg>
+        {}
+      </Link>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <div className='my-5'>
+        <div>Khoảng giá</div>
+        <form className='mt-2' onSubmit={onSubmit}>
+          <div className='flex items-start'>
+            <Controller
+              control={control}
+              name='price_min'
+              render={({ field }) => {
+                return (
+                  <InputNumber
+                    type='text'
+                    className='grow'
+                    placeholder='₫ TỪ'
+                    classNameInput='p-1 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                    classNameError='hidden'
+                    {...field}
+                    onChange={(event) => {
+                      field.onChange(event)
+                      trigger('price_max')
+                    }}
+                  />
+                )
+              }}
+            />
+
+            <div className='mx-2 mt-2 shrink-0'>-</div>
+            <Controller
+              control={control}
+              name='price_max'
+              render={({ field }) => {
+                return (
+                  <InputNumber
+                    type='text'
+                    className='grow'
+                    placeholder='₫ ĐẾN'
+                    classNameInput='p-1 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                    classNameError='hidden'
+                    {...field}
+                    onChange={(event) => {
+                      field.onChange(event)
+                      trigger('price_min')
+                    }}
+                  />
+                )
+              }}
+            />
+          </div>
+          <div className='mt-1 min-h-[1.25rem] text-center text-sm text-red-600'>{errors.price_min?.message}</div>
+          <Button className='flex w-full items-center justify-center bg-orange-500 p-2 text-sm uppercase text-white hover:bg-orange-600'>
+            Áp dụng
           </Button>
-        </div>
+        </form>
       </div>
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <div className='text-sm'>Đánh giá</div>
+      <RatingStarts queryConfig={queryConfig} />
+      <div className='my-4 h-[1px] bg-gray-300' />
+      <Button
+        onClick={handleRemoveAll}
+        className='flex w-full items-center justify-center bg-orange-500 p-2 text-sm uppercase text-white hover:bg-orange-600'
+      >
+        Xóa tất cả
+      </Button>
     </div>
   )
 }
