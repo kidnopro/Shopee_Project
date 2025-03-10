644cb37fae8293ba814a60029b8f7517115dc372
kidnopro <hoanganhfullstack@gmail.com>
2025-03-10 12:45:00 +0700

    feature: add i18 tranlete

--- OneDrive/Máy tính/ByKidNoPro/Shopee/ShopeeClone/src/i18n/i18n.ts
+++ OneDrive/Máy tính/ByKidNoPro/Shopee/ShopeeClone/src/i18n/i18n.ts
@@ -1,5 +1,36 @@
-import React from 'react'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HOME_EN from 'src/locales/en/home.json'
+import PRODUCT_EN from 'src/locales/en/product.json'
+import HOME_VI from 'src/locales/vi/home.json'
+import PRODUCT_VI from 'src/locales/vi/product.json'
 
-export default function i18n() {
-  return <div>i18n</div>
-}
+export const locales = {
+  en: 'English',
+  vi: 'Tiếng Việt'
+} as const
+
+export const resources = {
+  en: {
+    home: HOME_EN,
+    product: PRODUCT_EN
+  },
+  vi: {
+    home: HOME_VI,
+    product: PRODUCT_VI
+  }
+} as const
+
+export const defaultNS = 'product'
+
+// eslint-disable-next-line import/no-named-as-default-member
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'vi',
+  ns: ['home', 'product'],
+  fallbackLng: 'vi',
+  defaultNS,
+  interpolation: {
+    escapeValue: false
+  }
+})
